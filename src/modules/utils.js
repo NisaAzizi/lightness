c050ae85652e37e39df7c70a50d9e4bcce3c780d
Nisa Azizi <nisa.azizi@heig-vd.ch>
2023-01-27 08:30:52 +0100

    Initial commit

---
+++ src/modules/utils.js
@@ -0,0 +1,12 @@
+import * as convert from "color-convert";
+
+export const generatePalette = (hex) => {
+    const colors = [];
+    const couleurDeBase = convert.hex.hsl(hex);
+    for (i = 0;i+=10; i =100){
+
+    }
+
+
+
+}
